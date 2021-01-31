6a57b31c0dd1435b3a1fadee05e13c554d4c2556
ksbasyigit <49840672+ksbasyigit@users.noreply.github.com>
2021-01-31 22:21:56 +0300

    Update Program.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -9,42 +9,6 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
-            Car car1 = new Car() { Id = 6, BrandId = 3, ColorId = 2, ModelYear = "2001", DailyPrice = 1500, Description = "WV Caddy" };
-            Car car2 = new Car() { Id = 3, BrandId = 3, ColorId = 1, ModelYear = "2001", DailyPrice = 1600, Description = "WV Tiguan" };
-
-
-
-            carManager.Add(car1);
-            carManager.Delete(car1);
-            carManager.Update(car2);
-
-
-            CarManager carManagerGetAll = new CarManager(new InMemoryCarDal());
-            foreach (var car in carManagerGetAll.GetAll())
-            {
-                Console.WriteLine(car.Id);
-                Console.WriteLine(car.BrandId);
-                Console.WriteLine(car.ColorId);
-                Console.WriteLine(car.ModelYear);
-                Console.WriteLine(car.DailyPrice);
-                Console.WriteLine(car.Description);
-            }
-
-
-            Console.WriteLine("----------------GetById-----------------");
-            var getById = carManager.GetById(2);
-            foreach (var car in getById)
-            {
-                Console.WriteLine(car.Id);
-                Console.WriteLine(car.BrandId);
-                Console.WriteLine(car.ColorId);
-                Console.WriteLine(car.ModelYear);
-                Console.WriteLine(car.DailyPrice);
-                Console.WriteLine(car.Description);
-
-            }
-
             //Console.WriteLine("Hello World!");
         }
     }
